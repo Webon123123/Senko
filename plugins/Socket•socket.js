1fadff6ad9ad634a9db31694436f6304d8a70d31
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-12-16 01:01:55 -0500

    Add files via upload

---
+++ plugins/Socket•socket.js
@@ -0,0 +1,235 @@
+
+function _0x2fa4(e, o) {
+  let n = _0x407e();
+  return (_0x2fa4 = function(e, o) {
+    return e -= 218, n[e];
+  })(e, o);
+}
+
+let _0x3c2910 = _0x2fa4, {
+  useMultiFileAuthState,
+  DisconnectReason,
+  makeCacheableSignalKeyStore,
+  fetchLatestBaileysVersion
+} = ((() => {
+  for (var e = _0x2fa4, o = _0x407e(); ;) try {
+    if (171167 == +parseInt(e(279)) + -parseInt(e(289)) / 2 * (parseInt(e(281)) / 3) + -parseInt(e(291)) / 4 * (-parseInt(e(320)) / 5) + parseInt(e(251)) / 6 + -parseInt(e(327)) / 7 + -parseInt(e(300)) / 8 + parseInt(e(218)) / 9) break;
+    o.push(o.shift());
+  } catch (e) {
+    o.push(o.shift());
+  }
+})(), await import(_0x3c2910(220)));
+
+import _0xa9e9 from "qrcode";
+
+import _0x3673bb from "node-cache";
+
+import _0x1a61c0 from "fs";
+
+import _0x3438ce from "path";
+
+import _0x356ee2 from "pino";
+
+import _0xce42c9 from "util";
+
+import * as _0x375e54 from "ws";
+
+let {
+  child,
+  spawn,
+  exec
+} = await import(_0x3c2910(273)), CONNECTING = _0x375e54.CONNECTING;
+
+function _0x407e() {
+  let e = [ "newsletterFollow", "🚩 Conectado con éxito!! Puede conectarse usando:", "38582FsekUB", "chat", "1672jPYSfn", "data", "sender", "\n⭐ *Versión del bot* » ", "error", "A7IG1kNXN1b", "✐ 𝗙𝗨𝗡𝗖𝗜𝗢𝗡 𝗦𝗘𝗥 𝗦𝗨𝗕 𝗕𝗢𝗧 \n\n*Con otro celular o en la PC escanea este QR para convertirte en un Sub Bot*\n\n`1` » Haga clic en los tres puntos en la esquina superior derecha\n\n`2` » Toca dispositivos vinculados\n\n`3` » Escanee este codigo QR para iniciar sesión\n\n🜸 *¡Este código QR expira en 45 segundos!*\n\n*🜸 senkobot, Hecho por @MashaOFC*", "profilePictureUrl", "payload", "909248fbhoVo", "connectionUpdate", "users", "utf-8", "forEach", "Chrome", "creds", "🥳 ¡Nuevo Sub-Bot conectado!", "message", "jid", "tags", "NjNmYmJjYzA1YmFiY2MzZGU4MGRlICBpbmZvLWJvdC5qcwo", "channelid", "toString", "getTime", "🚩 Omitiendo mensajes en espera.", "fromMe", "silent", "base64", "split", "2385juprDI", "stringify", "restartRequired", "IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz", "Ubuntu", "serbot", "110.0.5585.95", "1418795cDQUzk", "UzYTI1MTQgIGluZ", "SBpbmZvLWRvbmFyLmpz", "output", "isBanned", "1535382mVRmAM", "registered", "@whiskeysockets/baileys", "credsUpdate", "mentionedJid", "splice", "chats", "parse", "image", "values", "\n🚩 Conexión perdida con el servidor, reconectando...", "conns", "indexOf", "serbot code", "jadibotmd", "log", "badSession", "removeAllListeners", "reply", "../handler.js?update=", "open", "keys", "DcgIF9hdXRvcmVzcG9uZGVyLmpzCjU5Yzc0ZjFjNmEz", "help", "loadMessage", "close", "statusCode", "writeFileSync", "NzZjM2ZmMzU2MTEyMzM3OTczOWU5ZmFmMDZjYzUzO", "trim", "/creds.json", "test", "https://files.catbox.moe/272vdy.jpg", "1074990NsgXJM", "ZThkMmNkOGVlMDFmZD", "from", "readFileSync", "socket", "jadibot", "unlinkSync", "\n\n> *¡Conviértete en sub-bot ahora!*\nwa.me/", "length", "Y2QgcGx1Z2lucy", "pushName", "sendMessage", "bind", "2.0.0", "creds.update", "off", "isInit", "\n💫 *Versión sub bot* » ", "catch", "remoteJid", "【 🔔 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗖𝗜𝗢́𝗡 🔔 】", "handler", "child_process", "existsSync", "user", "🚩 La conexión se ha cerrado de manera inesperada, intentaremos reconectar...", "👤 *Dueño* » ", "toBuffer", "227724bZYbXX", "messages.upsert", "45qpEXFb", "Senko-Bot (Sub Bot)", "\n⚠️❗ Razon de la desconexión desconocida: ", "Código de 8 dígitos", "mkdirSync", "connectionLost" ];
+  return (_0x407e = function() {
+    return e;
+  })();
+}
+
+import {
+  makeWASocket
+} from "../lib/simple.js";
+
+let check1 = "NjBhZGVmZWI4N2M2", check2 = _0x3c2910(252), check3 = _0x3c2910(328), check4 = "m8tZG9uYXIuanMK", check5 = _0x3c2910(246), check6 = _0x3c2910(240), check8 = _0x3c2910(311), crm1 = _0x3c2910(260), crm2 = _0x3c2910(296), crm3 = _0x3c2910(329), crm4 = _0x3c2910(323), drm1 = "", drm2 = "", rtx = _0x3c2910(297), rtx2 = "✐ 𝗙𝗨𝗡𝗖𝗜𝗢𝗡 𝗦𝗘𝗥 𝗦𝗨𝗕 𝗕𝗢𝗧 \n\n*🜸 Usa este Código para convertirte en un Sub Bot*\n\n`1` » Haga clic en los tres puntos en la esquina superior derecha\n\n`2` » Toca dispositivos vinculados\n\n`3` » Selecciona Vincular con el número de teléfono\n\n`4` » Escriba el Código\n\n🜸 *¡Este código solo funciona en el número que lo solicitó!*\n\n*🜸 Jadibot, Hecho por @Aiden_NotLogic*", handler = (global[_0x3c2910(229)] instanceof Array ? console[_0x3c2910(233)]() : global[_0x3c2910(229)] = [], 
+async (x, {
+  conn: e,
+  args: b,
+  usedPrefix: g,
+  command: h
+}) => {
+  let a = _0x3c2910;
+  if (!global.db[a(292)].settings[e[a(275)].jid][a(232)]) return e[a(236)](x[a(290)], "", x, rcanal);
+  let _ = e, v = !(!b[0] || !/(--code|code)/[a(249)](b[0].trim())) || !(!b[1] || !/(--code|code)/.test(b[1][a(247)]())), y, w, j, o = x[a(222)] && x[a(222)][0] ? x[a(222)][0] : x.fromMe ? _[a(275)][a(309)] : x[a(293)], n = (global.db.data[a(302)][x[a(293)]], 
+  "" + o[a(319)]`@`[0]);
+  v && (b[0] = b[0].replace(/^--code$|^code$/, "").trim(), b[1] && (b[1] = b[1].replace(/^--code$|^code$/, "").trim()), 
+  "" == b[0]) && (b[0] = void 0), _0x1a61c0[a(274)]("./" + jadi + "/" + n) || _0x1a61c0[a(285)]("./" + jadi + "/" + n, {
+    recursive: !0
+  }), b[0] && null != b[0] && _0x1a61c0.writeFileSync("./" + jadi + "/" + n + a(248), JSON[a(321)](JSON[a(225)](Buffer.from(b[0], "base64")[a(313)](a(303))), null, "\t")), 
+  _0x1a61c0[a(274)]("./" + jadi + "/" + n + a(248)) && (e = JSON[a(225)](_0x1a61c0.readFileSync("./" + jadi + "/" + n + "/creds.json"))) && (e[a(219)] = !1);
+  e = Buffer[a(253)](crm1 + crm2 + crm3 + crm4, "base64");
+  exec(e[a(313)]("utf-8"), async (e, o, n) => {
+    let p = a, f = Buffer[p(253)](drm1 + drm2, "base64");
+    !async function c() {
+      let i = p, l = x.mentionedJid && x[i(222)][0] ? x.mentionedJid[0] : x[i(316)] ? _.user.jid : x[i(293)], d = global.db[i(292)][i(302)][x[i(293)]], u = "" + l[i(319)]`@`[0];
+      _0x1a61c0[i(274)]("./" + jadi + "/" + u) || _0x1a61c0[i(285)]("./" + jadi + "/" + u, {
+        recursive: !0
+      }), b[0] && _0x1a61c0[i(245)]("./" + jadi + "/" + u + i(248), JSON.stringify(JSON[i(225)](Buffer[i(253)](b[0], i(318)).toString(i(303))), null, "\t"));
+      var {} = await fetchLatestBaileysVersion();
+      let e = new _0x3673bb(), {
+        state: o,
+        saveCreds: a
+      } = await useMultiFileAuthState("./" + jadi + "/" + u), t = {
+        printQRInTerminal: !1,
+        logger: _0x356ee2({
+          level: i(317)
+        }),
+        auth: {
+          creds: o[i(306)],
+          keys: makeCacheableSignalKeyStore(o.keys, _0x356ee2({
+            level: i(317)
+          }))
+        },
+        msgRetry: e => {},
+        msgRetryCache: e,
+        version: [ 2, 3e3, 1015901307 ],
+        syncFullHistory: !0,
+        browser: v ? [ i(324), "Chrome", i(326) ] : [ i(282), i(305), i(264) ],
+        defaultQueryTimeoutMs: void 0,
+        getMessage: async e => {
+          var o = i;
+          return store ? store[o(242)](e[o(270)], e.id)[o(308)] && void 0 : {
+            conversation: "SenkoBot"
+          };
+        }
+      }, m = makeWASocket(t), r = !(m[i(267)] = !1);
+      setInterval(async () => {
+        var o = i;
+        if (!m.user) {
+          try {
+            m.ws[o(243)]();
+          } catch (e) {
+            console[o(233)](await n(!0)[o(269)](console.error));
+          }
+          m.ev[o(235)](), (o = global[o(229)][o(230)](m)) < 0 || (delete global.conns[o], 
+          global.conns.splice(o, 1));
+        }
+      }, 6e4);
+      let s = await import("../handler.js"), n = async function(e) {
+        let n = i;
+        try {
+          var o = await import(n(237) + Date.now()).catch(console.error);
+          Object.keys(o || {})[n(259)] && (s = o);
+        } catch (e) {
+          console.error(e);
+        }
+        if (e) {
+          o = m[n(224)];
+          try {
+            m.ws.close();
+          } catch {}
+          m.ev[n(235)](), m = makeWASocket(t, {
+            chats: o
+          }), r = !0;
+        }
+        return r || (m.ev.off(n(280), m[n(272)]), m.ev[n(266)]("connection.update", m[n(301)]), 
+        m.ev.off(n(265), m[n(221)])), e = new Date(), o = new Date(1e3 * m.ev), 
+        e[n(314)]() - o.getTime() <= 3e5 ? (console.log("Leyendo mensajes entrantes:", m.ev), 
+        Object[n(239)](m[n(224)])[n(304)](e => {
+          var o = n;
+          m[o(224)][e].isBanned = !1;
+        })) : (console.log(m.chats, n(315), m.ev), Object.keys(m[n(224)])[n(304)](e => {
+          var o = n;
+          m[o(224)][e][o(331)] = !0;
+        })), m[n(272)] = s[n(272)][n(263)](m), m[n(301)] = async function(e) {
+          let n = i, {
+            connection: o,
+            lastDisconnect: a,
+            isNewLogin: t,
+            qr: r
+          } = e;
+          t && (m[n(267)] = !1), r && !v && (j = await _.sendMessage(x[n(290)], {
+            image: await _0xa9e9[n(278)](r, {
+              scale: 8
+            }),
+            caption: rtx + "\n" + f[n(313)]("utf-8")
+          }, {
+            quoted: x
+          })), r && v && (y = await _[n(262)](x[n(290)], {
+            text: rtx2 + "\n" + f.toString(n(303))
+          }, {
+            quoted: x
+          }), await sleep(3e3), e = await m.requestPairingCode(x[n(293)].split`@`[0]), 
+          w = await x[n(236)](e));
+          var e = a?.[n(295)]?.output?.statusCode || a?.[n(295)]?.[n(330)]?.payload?.statusCode, e = (console[n(233)](e), 
+          async e => {
+            var o = n;
+            if (!e) {
+              try {
+                m.ws[o(243)]();
+              } catch {}
+              m.ev[o(235)](), (e = global[o(229)][o(230)](m)) < 0 || (delete global[o(229)][e], 
+              global.conns[o(223)](e, 1));
+            }
+          }), s = a?.[n(295)]?.[n(330)]?.[n(244)] || a?.[n(295)]?.[n(330)]?.[n(299)]?.statusCode;
+          if (o === n(243)) {
+            if (console[n(233)](s), 405 == s) return await _0x1a61c0[n(257)]("./" + jadi + "/" + u + n(248)), 
+            x.reply("🚩 Reenvia nuevamente el comando.");
+            if (s === DisconnectReason[n(322)]) return c(), console[n(233)]("\n⏳️ Tiempo de la conexión agotado, reconectando...");
+            if (s === DisconnectReason.loggedOut) return sleep(4e3), x[n(236)]("🚩 *La conexión se ha cerrado, tendras que volver a conectarse usando:*\n!deletesesion (Para borrar los datos y poder volver a solicitar el QR o el código de emparejamiento");
+            if (428 == s) return await e(!1), x[n(236)](n(276));
+            if (s === DisconnectReason[n(286)]) return await c(), console.log(n(228));
+            if (s === DisconnectReason[n(234)]) return x[n(236)]("🚩 La conexión se ha cerrado, deberás conectarse manualmente.");
+            if (s === DisconnectReason.timedOut) return await e(!1), console[n(233)]("\n⏳️ Tiempo de la conexión agotado, reconectando...");
+            console[n(233)](n(283) + (s || "") + " >> " + (o || ""));
+          }
+          null == global.db[n(292)] && loadDatabase(), o == n(238) && (m[n(267)] = !0, 
+          global[n(229)].push(m), await joinChannels(m), await _[n(262)](x[n(290)], {
+            text: b[0] ? "⚪ *Está conectado(a)!! Por favor espere se está cargando los mensajes...*\n\n♻️ *Opciones Disponibles:*\n*» .pausarai _(Detener la función Sub Bot)_*\n*» .deletesession _(Borrar todo rastro de Sub Bot)_*\n*» .serbot _(Nuevo código QR o Conectarse si ya es Sub Bot)_*" : n(288) + " " + (g + h)
+          }, {
+            quoted: x
+          }), e = await m[n(298)](l, n(226))[n(269)](e => n(250)), s = (n(277) + (x[n(261)] || "Anónimo") + "\n🗃️ *Registrado* » " + (d[n(219)] ? "Si\n✅ *Verificación* » " + d.name : "No") + "\n🔑 *Método de conexión* » " + (v ? n(284) : "Código QR") + "\n💻 *Browser* » " + (v ? n(324) : n(305)) + n(294) + vs + n(268) + vsJB + n(258) + l[n(319)]`@`[0] + "?text=.serbot%20code")[n(247)](), 
+          await sleep(3e3), await _[n(262)](global[n(312)], {
+            text: s,
+            contextInfo: {
+              externalAdReply: {
+                title: n(271),
+                body: n(307),
+                thumbnailUrl: e,
+                sourceUrl: redes,
+                mediaType: 1,
+                showAdAttribution: !1,
+                renderLargerThumbnail: !1
+              }
+            }
+          }, {
+            quoted: null
+          }), b[0] || _.sendMessage(x[n(293)], {
+            text: g + h + " " + Buffer[n(253)](_0x1a61c0[n(254)]("./" + jadi + "/" + u + n(248)), n(303))[n(313)](n(318))
+          }, {
+            quoted: x
+          }));
+        }[n(263)](m), m[n(221)] = a[n(263)](m, !0), m.ev.on(n(280), m[n(272)]), 
+        m.ev.on("connection.update", m.connectionUpdate), m.ev.on(n(265), m[n(221)]), 
+        !(r = !1);
+      };
+      n(!1);
+    }();
+  });
+});
+
+handler[_0x3c2910(241)] = [ _0x3c2910(325), _0x3c2910(231) ], handler[_0x3c2910(310)] = [ _0x3c2910(255) ], 
+handler.command = [ _0x3c2910(256), _0x3c2910(325) ];
+
+export default handler;
+
+let delay = o => new Promise(e => setTimeout(e, o));
+
+function sleep(o) {
+  return new Promise(e => setTimeout(e, o));
+}
+
+async function joinChannels(e) {
+  var o, n = _0x3c2910;
+  for (o of Object[n(227)](global.ch)) await e[n(287)](o)[n(269)](() => {});
+}
